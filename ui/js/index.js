cf633e0c03d80ea746e522945861be5577fc8d03
andrei <andrei.ungur@mail.mcgill.ca>
2018-01-20 19:41:20 -0500

    Improve drawing in UI

--- ui/js/index.js
+++ ui/js/index.js
@@ -1,107 +1,66 @@
-var canvas,
-  ctx,
-  flag = false,
-  prevX = 0,
-  currX = 0,
-  prevY = 0,
-  currY = 0,
-  dot_flag = false;
+var canvas = document.getElementById('main_image'),
+  ctx = canvas.getContext('2d'),
+  mouseX = 0,
+  mouseY = 0,
+  width = canvas.width,
+  height = canvas.height,
+  colour = 'hotpink',
+  mousedown = false;
 
-var color = "black",
-  y = 2;
-function init() {
-  canvas = document.getElementById("can");
-  ctx = canvas.getContext("2d");
-  w = canvas.width;
-  h = canvas.height;
+console.log("width: "+width);
+console.log("height: "+height);
 
-  canvas.addEventListener(
-    "mousemove",
-    function(e) {
-      findxy("move", e);
-    },
-    false
-  );
+// resize the canvas
+canvas.width = width;
+canvas.height = height;
 
-  canvas.addEventListener(
-    "mousedown",
-    function(e) {
-      findxy("down", e);
-    },
-    false
-  );
+var stroke_color = "black",
+  stroke_width = 10;
 
-  canvas.addEventListener(
-    "mouseup",
-    function(e) {
-      findxy("up", e);
-    },
-    false
-  );
 
-  canvas.addEventListener(
-    "mouseout",
-    function(e) {
-      findxy("out", e);
-    },
-    false
-  );
-}
+canvas.addEventListener('mousemove', function (event) {
+  if (event.offsetX) {
+    mouseX = event.offsetX;
+    mouseY = event.offsetY;
+  } else {
+    mouseX = event.pageX - event.target.offsetLeft;
+    mouseY = event.pageY - event.target.offsetTop;
+  }
+  // call the draw function
+  draw();
+}, false);
+
+canvas.addEventListener('mousedown', function (event) {
+  mousedown = true;
+}, false);
+canvas.addEventListener('mouseup', function (event) {
+  mousedown = false;
+}, false);
 
 function draw() {
-  ctx.beginPath();
-  ctx.moveTo(prevX, prevY);
-  ctx.lineTo(currX, currY);
-  ctx.strokeStyle = x;
-  ctx.lineWidth = y;
-  ctx.stroke();
-  ctx.closePath();
+  if (mousedown) {
+    // set the colour
+    ctx.fillStyle = stroke_color;
+    // start a path and paint a circle of 20 pixels at the mouse position
+    ctx.beginPath();
+    ctx.arc(mouseX, mouseY, stroke_width, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fill();
+  }
 }
 
 function erase() {
-  var m = confirm("Want to clear");
+  var m = confirm("Vous voulez effacer?");
   if (m) {
-    ctx.clearRect(0, 0, w, h);
-    document.getElementById("canvasimg").style.display = "none";
+    ctx.clearRect(0, 0, width, height);
   }
 }
 
-function save() {
-  document.getElementById("canvasimg").style.border = "2px solid";
+function analyze() {
+  document.getElementById("main_image").style.border = "2px solid";
   var dataURL = canvas.toDataURL();
-  document.getElementById("canvasimg").src = dataURL;
-  document.getElementById("canvasimg").style.display = "inline";
-}
+  console.log(dataURL);
 
-function findxy(res, e) {
-  if (res == "down") {
-    prevX = currX;
-    prevY = currY;
-    currX = e.clientX - canvas.offsetLeft;
-    currY = e.clientY - canvas.offsetTop;
-
-    flag = true;
-    dot_flag = true;
-    if (dot_flag) {
-      ctx.beginPath();
-      ctx.fillStyle = x;
-      ctx.fillRect(currX, currY, 2, 2);
-      ctx.cl;
-
-      osePath();
-      dot_flag = false;
-    }
-  }
-  if (res == "up" || res == "out") {
-    flag = false;
-  }
-  if (res == "move") {
-    if (flag) {
-      prevX = currX;
-      prevY = currY;
-      currX = e.clientX - canvas.offsetLeft;
-      currY = e.clientY - canvas.offsetTop;
-      draw();
-    }
-  }
+  //This is gonna contain the output from the API
+  document.getElementById('interpretation').innerHTML= 'asdasdasd';
 }
